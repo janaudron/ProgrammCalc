37bafda695fa8edfee7ab81f3edaca21cef3c2d3
Aleksei Somikov <jaudron@yandex.ru>
2017-11-06 17:07:27 +0300

    Delete extra methods from class NumberCE [Issue #8] After this commit, class NumberCE only stores the number

--- src/number_types/NumberCE.java
+++ src/number_types/NumberCE.java
@@ -18,15 +18,6 @@ public class NumberCE {
     private double num = 0.0;
 
     /**
-     * Max value for this type
-     */
-    final public double MAX_VALUE = Double.MAX_VALUE;
-    /**
-     * Min value for this type
-     */
-    final public double MIN_VALUE = Double.MIN_VALUE;
-
-    /**
      * Sets the value for the number
      *
      * @param value - settable value
@@ -43,29 +34,4 @@ public class NumberCE {
     public double getValue() {
         return this.num;
     }
-
-    /**
-     * Get hex string of the number
-     *
-     * @return string of the number
-     */
-    public String toHex() {
-        long long_bits = Double.doubleToLongBits(this.num);
-        String str_hex = Long.toHexString(long_bits);
-        str_hex = String.format("0x%s", str_hex);
-        return str_hex;
-    }
-
-    /**
-     * Get bin string of the number
-     *
-     * @return string of the number
-     */
-    public String toBin() {
-        long long_bits = Double.doubleToLongBits(this.num);
-        String str_bin = Long.toBinaryString(long_bits);
-//        str_bin = String.format("%064s", str_bin);
-        return str_bin;
-    }
-
 }
--- test/number_types/NumberCETest.java
+++ test/number_types/NumberCETest.java
@@ -17,7 +17,6 @@ import static org.junit.Assert.*;
  * @author JAudron
  */
 public class NumberCETest {
-
     private int error_count;
     private int case_count;
     private static int test_indx = 0;
@@ -56,7 +55,7 @@ public class NumberCETest {
     @Test
     public void testSetValue() {
         System.out.println("Test NumberCE.setValue");
-        double[] value_buf = {-171.0, 171.0};
+        double[] value_buf = {-171.0, 0.0, 171.0};
         NumberCE inst = new NumberCE();
         for (double val : value_buf) {
             case_count++;
@@ -75,7 +74,7 @@ public class NumberCETest {
     @Test
     public void testGetValue() {
         System.out.println("Test NumberCE.getValue");
-        double[] value_buf = {-171.0, 171.0};
+        double[] value_buf = {-171.0, 0.0, 171.0};
         NumberCE inst = new NumberCE();
         for (double val : value_buf) {
             case_count++;
@@ -85,69 +84,6 @@ public class NumberCETest {
             if (val != ref_value) {
                 error_count++;
             }
-        }
-    }
-
-    /**
-     * Test of toHex method, of class NumberCE.
-     */
-    @Test
-    public void testToHex() {
-        System.out.println("Test NumberCE.toHex");
-        double[] value_buf = {
-            171.0,
-            0.0,
-            -171.0,
-            353.353
-        };
-        String[] string_buf = {
-            "0x4065600000000000",
-            "0x0",
-            "0xc065600000000000",
-            "0x407615a5e353f7cf"
-        };
-        NumberCE inst = new NumberCE();
-        for (double val : value_buf) {
-
-            inst.setValue(val);
-            String inst_hex = inst.toHex();
-
-            if (!inst_hex.equals(string_buf[case_count])) {
-                error_count++;
-            }
-
-            case_count++;
-        }
-    }
-
-    /**
-     * Test of toBin method, of class NumberCE.
-     */
-    @Test
-    public void testToBin() {
-        System.out.println("Test NumberCE.toBin");
-        double[] value_buf = {
-            171.0,
-            0.0,
-            -171.0,
-            353.353
-        };
-        String[] string_buf = {
-            "100000001100101011000000000000000000000000000000000000000000000",
-            "0",
-            "1100000001100101011000000000000000000000000000000000000000000000",
-            "100000001110110000101011010010111100011010100111111011111001111"
-        };
-        NumberCE inst = new NumberCE();
-        for (double val : value_buf) {
-            inst.setValue(val);
-            String inst_bin = inst.toBin();
-
-            if (!inst_bin.equals(string_buf[case_count])) {
-                error_count++;
-            }
-
-            case_count++;
         }
     }
 
